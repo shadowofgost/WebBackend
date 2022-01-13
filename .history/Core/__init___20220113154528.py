1a60d9032c597f4308c9800b62298dd77043cf2b
shadowofgost <shadowofgost@outlook.com>
2022-01-13 16:03:56 +0800

    Init

---
+++ .history/Core/__init___20220113154528.py
@@ -0,0 +1,12 @@
+"""
+# @Time             : 2022-01-13 15:35:27
+# @Author           : Albert Wang
+# @Email            : shadowofgost@outlook.com
+# @Software         : Vscode
+# @FilePath         : /WebBackend/Core/__init__.py
+# @Copyright Notice : Copyright (c) ${now_year} Albert Wang 王子睿, All Rights Reserved.
+# @Copyright (c) 2022 Albert Wang 王子睿, All Rights Reserved.
+# @Description      :
+# @LastTime         : 2022-01-13 15:35:28
+# @LastAuthor       : Albert Wang
+"""
