faf994da2fc408d48a4177e621c55c3e061d0f2b
shadowofgost <shadowofgost@outlook.com>
2022-02-17 20:58:15 +0800

    重建git提交目录

--- Backup/DepartmentModel/Serializers.py
+++
@@ -1,6 +0,0 @@
-from rest_framework.serializers import ModelSerializer
-from .Database import DepartmentModel
-class DepartmentSerializer(ModelSerializer):
-    class Meta:
-        model=DepartmentModel
-        fields="__all__"
